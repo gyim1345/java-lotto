d142bda123bc0d822643605d01e1b8dd3a3c5421
gyim1345 <gyim1345@gmail.com>
2020-06-18 14:22:46 +0900

    Add random generator for lottery tickets

--- src/main/java/lotto/step2/RandomNumber.java
+++ src/main/java/lotto/step2/RandomNumber.java
@@ -1,4 +1,26 @@
 package lotto.step2;
 
+import java.util.Arrays;
+import java.util.Random;
+
 public class RandomNumber {
+
+    public static int[][] generateRandomNumberOfArraysOfArrays(int amountOfArraysofArrays, int amountOfNumbersOfArraysOfArrays, int maxRangeOfNumber) {
+        int[][] randomNumbersOfArraysOfArrays = new int[amountOfArraysofArrays][];
+        for(int j =0; j< amountOfArraysofArrays; j++ ) {
+            randomNumbersOfArraysOfArrays[j] = generateSortedRandomNumbersOfArray(amountOfNumbersOfArraysOfArrays,maxRangeOfNumber);
+        }
+        return randomNumbersOfArraysOfArrays;
+    }
+
+    private static int[] generateSortedRandomNumbersOfArray(int amount, int maxRange){
+        int[] randomNumbersOfArray = new int[amount];
+        for(int i=0; i<amount; i++) {
+            randomNumbersOfArray[i] = new Random().nextInt(maxRange);
+        }
+        Arrays.sort(randomNumbersOfArray);
+        return randomNumbersOfArray;
+    }
+    
+    
 }
