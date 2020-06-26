3fea33184512d723faed47658233a8f96f79a103
gyim1345 <gyim1345@gmail.com>
2020-06-26 18:58:05 +0900

    Edit result view to view data result

--- src/main/java/lotto/step4/ui/resultView.java
+++ src/main/java/lotto/step4/ui/resultView.java
@@ -1,4 +1,11 @@
 package lotto.step4.ui;
 
+import java.util.Arrays;
+
 public class resultView {
+    public static void showTickets(int[][] lottoArray) {
+        for(int i = 0; i < lottoArray.length; i ++) {
+            System.out.println(Arrays.toString(lottoArray[i]));
+        }
+    }
 }
