f4e45870dd8ffa9dbff6701815f695e2e195a664
gyim1345 <gyim1345@gmail.com>
2020-07-01 17:30:35 +0900

    Add tests to CalculatorTest
    Check method rateOfReturnTest

--- src/test/java/lotto/step4/CalculatorTest.java
+++ src/test/java/lotto/step4/CalculatorTest.java
@@ -2,10 +2,13 @@ package lotto.step4;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertThat;
+
 public class CalculatorTest {
 
     @Test
     public void rateOfReturnTest() {
-
+        Calculator calculator = new Calculator();
+        assertThat(calculator.rateOfReturnTest(new int[] {10000, 100000, 1000, 0,0,})).isEqualTo(222);
     }
 }
