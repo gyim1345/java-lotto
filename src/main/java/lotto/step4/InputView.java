624dc81b62484c236f81befcf9cf9daee8552cb6
gyim1345 <gyim1345@gmail.com>
2020-07-01 19:47:42 +0900

    Add inputView to get Cash from user input
    Add getCashAmount to get inputs from scanner

--- src/main/java/lotto/step4/InputView.java
+++ src/main/java/lotto/step4/InputView.java
@@ -3,5 +3,10 @@ package lotto.step4;
 import java.util.Scanner;
 
 public class InputView {
-    Scanner scanner;
+    private static Scanner scanner;
+
+    public static int getCashAmount() {
+        scanner = new Scanner(System.in);
+        return scanner.nextInt();
+    }
 }
