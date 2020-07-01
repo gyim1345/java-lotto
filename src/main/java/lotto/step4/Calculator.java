6c6c0842f19fba51462507145d99c4d5742c2cca
gyim1345 <gyim1345@gmail.com>
2020-07-01 18:19:42 +0900

    Refactor Calculator and its test
    change to static method
    change method name rateOfReturnTest => rateOfReturn

--- src/main/java/lotto/step4/Calculator.java
+++ src/main/java/lotto/step4/Calculator.java
@@ -3,7 +3,7 @@ package lotto.step4;
 import java.text.DecimalFormat;
 
 public class Calculator {
-    public String rateOfReturnTest(int totalProfit, int spentCash) {
+    public static String rateOfReturn(int totalProfit, int spentCash) {
         DecimalFormat df = new DecimalFormat("#.##");
         return df.format((double) totalProfit/spentCash);
     }
