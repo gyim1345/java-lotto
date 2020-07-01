d6f9b19e41d39d4b0916e25f1f4135bb0b7b9bd3
gyim1345 <gyim1345@gmail.com>
2020-07-01 18:05:45 +0900

    refactor test
    change parameters and return type

---
+++ src/main/java/lotto/step4/Calculator.java
@@ -0,0 +1,4 @@
+package lotto.step4;
+
+public class Calculator {
+}
--- src/test/java/lotto/step4/CalculatorTest.java
+++ src/test/java/lotto/step4/CalculatorTest.java
@@ -1,14 +1,15 @@
 package lotto.step4;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.assertThat;
-
+//import static org.junit.Assert.assertThat;
+import static org.assertj.core.api.Assertions.assertThat;
 public class CalculatorTest {
 
     @Test
     public void rateOfReturnTest() {
         Calculator calculator = new Calculator();
-        assertThat(calculator.rateOfReturnTest(new int[] {10000, 100000, 1000, 0,0,})).isEqualTo(222);
+        assertThat(calculator.rateOfReturnTest(10000, 14000)).isEqualTo(0.714285714);
+        assertThat(calculator.rateOfReturnTest(14000, 14000)).isEqualTo(1);
     }
 }
