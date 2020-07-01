977ddd4d6b7b591445d0bbaaffa0a1d6f74eeabe
gyim1345 <gyim1345@gmail.com>
2020-07-01 18:16:37 +0900

    Finish rateOfReturn of Calculator
    Calculates rate of return

--- src/main/java/lotto/step4/Calculator.java
+++ src/main/java/lotto/step4/Calculator.java
@@ -1,4 +1,10 @@
 package lotto.step4;
 
+import java.text.DecimalFormat;
+
 public class Calculator {
+    public String rateOfReturnTest(int totalProfit, int spentCash) {
+        DecimalFormat df = new DecimalFormat("#.##");
+        return df.format((double) totalProfit/spentCash);
+    }
 }
