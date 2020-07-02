158b71b98775490dab1786124ac237f6e2708527
gyim1345 <gyim1345@gmail.com>
2020-07-02 18:12:56 +0900

    Finish Cash class
    Add constructor
    Add validator

--- src/main/java/lotto/step4/Cash.java
+++ src/main/java/lotto/step4/Cash.java
@@ -1,6 +1,18 @@
 package lotto.step4;
 
 public class Cash {
-    public Cash(int i) {
+    private final int cash;
+
+    public Cash(int cash) {
+        validate(cash);
+        this.cash = cash;
     }
+
+    private void validate(int cash) {
+        if(cash<1000) {
+            throw new IllegalArgumentException("1000원 이상 넣어 주세요");
+        }
+    }
+
+
 }
