c27bb00e7521b71656e6ce4584288d3f5d6cdabf
gyim1345 <gyim1345@gmail.com>
2020-07-02 19:04:48 +0900

    Add new test, testcase, refactor
    Add TotalProftitTest to get total profit
    Add another test case for rateOfReturn
    Erase unused imports

--- src/test/java/lotto/step4/CalculatorTest.java
+++ src/test/java/lotto/step4/CalculatorTest.java
@@ -1,9 +1,8 @@
 package lotto.step4;
 
 import org.junit.jupiter.api.Test;
-
-//import static org.junit.Assert.assertThat;
 import static org.assertj.core.api.Assertions.assertThat;
+
 public class CalculatorTest {
 
     @Test
@@ -13,5 +12,14 @@ public class CalculatorTest {
         assertThat(calculator.rateOfReturn(14000, 14000)).isEqualTo("1");
         assertThat(calculator.rateOfReturn(5000, 14000)).isEqualTo("0.35");
         assertThat(calculator.rateOfReturn(500000, 5000)).isEqualTo("100");
+        assertThat(calculator.rateOfReturn(0,100000)).isEqualTo("0");
+    }
+
+    @Test
+    public void TotalProfitTest() {
+        Calculator calculator = new Calculator();
+        assertThat(calculator.totalProfit(1000,10000,100000,100000)).isEqualTo(1111000);
+        assertThat(calculator.totalProfit(2000,20000,200000,200000)).isEqualTo(2222000);
+        assertThat(calculator.totalProfit(0,0,0,0)).isEqualTo(0);
     }
 }
