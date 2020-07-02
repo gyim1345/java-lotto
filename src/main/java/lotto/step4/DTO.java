c2936e85a164a9490f9081622352f10be70ac6c7
gyim1345 <gyim1345@gmail.com>
2020-07-02 18:36:42 +0900

    Make DTO class
    Add cash variable
    Add set method for cash
    Add get method for cash

--- src/main/java/lotto/step4/DTO.java
+++ src/main/java/lotto/step4/DTO.java
@@ -1,4 +1,18 @@
 package lotto.step4;
 
 public class DTO {
+    private int cash;
+
+    public DTO() {
+
+    }
+
+    public void setCash(Cash cash) {
+        this.cash = cash.getCash();
+    }
+
+    public int getCash() {
+        return this.cash;
+    }
+
 }
