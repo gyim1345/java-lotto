e6cc5efa80b4947087bd1082c086d89ff76e531d
gyim1345 <gyim1345@gmail.com>
2020-07-06 20:14:34 +0900

    Change hard coded codes to use created methods
    Use Cash to generate Tickets from lottoTicket class
    Save ticket info in dto
    Print tickets using info in dto

--- src/main/java/lotto/step4/DTO.java
+++ src/main/java/lotto/step4/DTO.java
@@ -4,6 +4,7 @@ public class DTO {
     private int cash;
     private int[] profits;
     private int[] rewards = {0, 0, 0, 5000, 50000, 1500000, 2000000000};
+    private int[][] lottoTickets;
 
     public DTO() {
 
@@ -35,5 +36,18 @@ public class DTO {
 
     public int getReward(int matchCount) {
         return this.rewards[matchCount];
+    }
+
+    public void setLottoTickets(LottoTickets lottoTickets) {
+        this.lottoTickets = new int[lottoTickets.getLottoTickets().length][lottoTickets.getLottoTickets()[0].length];
+        int index = 0;
+        for(int[] ticket: lottoTickets.getLottoTickets()) {
+            System.arraycopy(ticket,0,this.lottoTickets[index], 0,ticket.length);
+            index += 1;
+        }
+    }
+
+    public int[][] getLottoTickets() {
+        return this.lottoTickets;
     }
 }
--- src/main/java/lotto/step4/LottoGameApp.java
+++ src/main/java/lotto/step4/LottoGameApp.java
@@ -12,30 +12,16 @@ public class LottoGameApp {
         Cash cash = new Cash(InputView.getCashAmount());
         dto.setCash(cash);
 
-        int [][] lottoTickets = {
-                {8, 21, 23, 41, 42, 43},
-                {3, 5, 11, 16, 32, 38},
-                {7, 11, 16, 35, 36, 44},
-                {1, 8, 11, 31, 41, 42},
-                {13, 14, 16, 38, 42, 45},
-                {7, 11, 30, 40, 42, 43},
-                {2, 13, 22, 32, 38, 45},
-                {23, 25, 33, 36, 39, 41},
-                {1, 3, 5, 14, 22, 45},
-                {5, 9, 38, 41, 43, 44},
-                {2, 8, 9, 18, 19, 21},
-                {13, 14, 18, 21, 23, 35},
-                {17, 21, 29, 37, 42, 45},
-                {3, 8, 27, 30, 35, 44},
-                {1, 2, 3, 4, 5, 6}};
+        LottoTickets lottoTickets = new LottoTickets(LottoService.buyTickets(cash));
 
+        dto.setLottoTickets(lottoTickets);
 
-        for(int[] array : lottoTickets) {
+        for(int[] array : dto.getLottoTickets()) {
             System.out.println(Arrays.toString(array));
         }
 
         int [] lastWeekLottoPrizeNumber = {1,2,3,4,5,6};
-        int[] profits = LottoService.getProfits(lottoTickets, lastWeekLottoPrizeNumber);
+        int[] profits = LottoService.getProfits(dto.getLottoTickets(), lastWeekLottoPrizeNumber);
         int[] matchedProfits = LottoService.getMatchingProfits(profits);
         dto.setMatchingProfits(matchedProfits);
 
