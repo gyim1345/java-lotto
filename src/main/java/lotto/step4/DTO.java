797b30bb7d5c9a0488dd59e06cb0552094c6ba78
gyim1345 <gyim1345@gmail.com>
2020-07-06 17:18:58 +0900

    Add methods to send data to UI
    add setMatchingProfit to set profit
    add getMatchingProfit to get profit
    add getReward to get reward

--- src/main/java/lotto/step4/DTO.java
+++ src/main/java/lotto/step4/DTO.java
@@ -2,6 +2,8 @@ package lotto.step4;
 
 public class DTO {
     private int cash;
+    private int[] profits;
+    private int[] rewards = {0, 0, 0, 5000, 50000, 1500000, 2000000000};
 
     public DTO() {
 
@@ -15,4 +17,23 @@ public class DTO {
         return this.cash;
     }
 
+    public void setMatchingProfits(int[] matchedProfits) {
+        this.profits = matchedProfits;
+    }
+
+    public int[] getMatchingProfits() {
+        return this.profits;
+    }
+
+    public int getMatchingProfit(int index) {
+        return this.profits[index];
+    }
+
+    public int[] getRewards() {
+        return this.rewards;
+    }
+
+    public int getReward(int matchCount) {
+        return this.rewards[matchCount];
+    }
 }
