2310e693527d0a389f5aba4a7206071d18d961ee
gyim1345 <gyim1345@gmail.com>
2020-07-06 18:59:15 +0900

    Finish Lotto Tickets Test and its function
    hard code ticket and variables
    TICKET_MAX_SIZE = 6
    NUMBER_MAX = 45
    NUMBER_MIN = 1

--- src/main/java/lotto/step4/LottoTickets.java
+++ src/main/java/lotto/step4/LottoTickets.java
@@ -1,9 +1,14 @@
 package lotto.step4;
 
+import java.util.Arrays;
+
 public class LottoTickets {
 
     private int[][] LottoTickets;
-    private final int [][] LottoFixedTickets = {
+    private final int TICKET_MAX_SIZE = 6;
+    private final int NUMBER_MAX = 45;
+    private final int NUMBER_MIN = 1;
+    private int[][] LottoFixedTickets = {
             {8, 21, 23, 41, 42, 43},
             {3, 5, 11, 16, 32, 38},
             {7, 11, 16, 35, 36, 44},
@@ -19,17 +24,41 @@ public class LottoTickets {
             {17, 21, 29, 37, 42, 45},
             {3, 8, 27, 30, 35, 44},
             {1, 2, 3, 4, 5, 6}};
+
     public LottoTickets(int amount) {
+        this.LottoTickets = new int[amount][TICKET_MAX_SIZE];
+        generateTickets(amount);
+        verification();
+    }
+
+    public LottoTickets(int amount, int[][] fixedTickets) {
+        this.LottoFixedTickets = fixedTickets;
+        verification();
+        this.LottoTickets = new int[amount][TICKET_MAX_SIZE];
         generateTickets(amount);
     }
 
-    private int[][] generateTickets(int amount) {
-
-        for(int i = 0; i < amount; i += 1) {
-            this.LottoTickets[i] = this.LottoFixedTickets[i];
-        }
-
-        return this.LottoTickets = this.LottoFixedTickets;
+    public int[][] getLottoTickets() {
+        return this.LottoTickets;
     }
 
+    private void generateTickets(int amount) {
+        for (int i = 0; i < amount; i += 1) {
+            System.out.println(Arrays.toString(this.LottoFixedTickets[i]));
+            LottoTickets[i] = this.LottoFixedTickets[i];
+        }
+    }
+
+    private void verification() {
+            for (int[] ticket : this.LottoFixedTickets) {
+            if (ticket.length > TICKET_MAX_SIZE) {
+                throw new IllegalArgumentException("lotto ticket size must be 6");
+            }
+                for (int number : ticket) {
+                    if (number > NUMBER_MAX || number < NUMBER_MIN) {
+                        throw new IllegalArgumentException("lotto number size must be between 1 and 45");
+                    }
+                }
+            }
+    }
 }
--- src/test/java/lotto/step4/LottoTicketsTest.java
+++ src/test/java/lotto/step4/LottoTicketsTest.java
@@ -1,8 +1,30 @@
 package lotto.step4;
 
 import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class LottoTicketsTest {
     @Test
-    lottoT
+    public void LottoTicketTest() {
+        LottoTickets lottoTickets = new LottoTickets(5);
+        assertThat(lottoTickets.getLottoTickets().length).isEqualTo(5);
+    }
+
+    @Test
+    public void LottoTicketTest2() {
+        assertThatThrownBy(() -> {
+            new LottoTickets(5, new int [][] {{1,2,3,4,5,6,7}});
+        }).isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("lotto ticket size must be 6");
+    }
+
+    @Test
+    public void LottoTicketTest3() {
+        assertThatThrownBy(() -> {
+            new LottoTickets(5, new int [][] {{0,2,3,4,5,6,7}});
+        }).isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("lotto ticket size must be 6");
+    }
+
 }
